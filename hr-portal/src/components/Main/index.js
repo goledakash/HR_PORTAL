046d23673e1cec07ef21872c42cfb4af41740b43
Akash Goled <akashgoled@Akashs-MacBook-Pro.local>
2018-09-06 16:58:04 -0400

    Added Custom Trigger Layout in Main

--- hr-portal/src/components/Main/index.js
+++ hr-portal/src/components/Main/index.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import { connect } from "react-redux";
 import PropTypes from 'prop-types';
-import { Menu, Icon, List, Button, Tabs, Row, Col} from 'antd';
-import Header from '../Header';
+import { Menu, Icon, List, Button, Tabs, Row, Col, Layout} from 'antd';
+// import Header from '../Header';
 import { Link } from "react-router-dom";
 import { getEmployeeListFromFirebase } from "../../redux/actions/Main";
+
+const { Header, Sider, Content } = Layout;
 
 const TabPane = Tabs.TabPane;
 
@@ -48,71 +50,61 @@ class Main extends Component {
     render() { 
         const { taskList } = this.props;
         return ( 
-            <div>
-                <h2>Main</h2>
-                    {/* <Header /> */}
-                        <Row>
-                        <Col>
-                            <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
-                            
+            <Layout>
+
+            <Sider
+              trigger={null}
+              collapsible
+              collapsed={this.state.collapsed}
+            >
+                            {/* <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
                             <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
-                            </Button>
+                            </Button> */}
 
-                            <div style={{ width: 256 }}>
-                            <Menu
-                            defaultSelectedKeys={['1']}
-                            defaultOpenKeys={['sub1']}
-                            mode="inline"
-                            theme="light"
-                            inlineCollapsed={this.state.collapsed}
-                            >
-                                <Menu.Item key="1">
-                                    <Icon type="profile" theme="outlined" />
-                                    <span>
-                                        <Link
-                                        to={{
-                                        pathname: "/createTask"
-                                        }}>Create Task
-                                        </Link>
-                                    </span>
-                                </Menu.Item>
-                                <Menu.Item key="2">
-                                    <Icon type="desktop" />
-                                    <span>
-                                        <Link
-                                        to={{
-                                        pathname: "/empStatusUpdates"
-                                        }}>EmpStatusUpdates
-                                        </Link>
-                                    </span>
-                                </Menu.Item>
-                                
-                            </Menu>
+                            <div >
+                                <Menu
+                                defaultSelectedKeys={['1']}
+                                mode="inline"
+                                theme="light"
+                                >
+                                        <Menu.Item key="1">
+                                            <Icon type="profile" theme="outlined" />
+                                            <span>
+                                                <Link to={{pathname: "/createTask"}}>Create Task</Link>
+                                            </span>
+                                        </Menu.Item>
+                                        <Menu.Item key="2">
+                                            <Icon type="desktop" />
+                                            <span>
+                                                <Link to={{pathname: "/empStatusUpdates"}}>EmpStatusUpdates</Link>
+                                            </span>
+                                        </Menu.Item>
+                                </Menu>
                             </div>
-                        </Col>
+            </Sider>
 
-                        <Col>
-                            <Tabs defaultActiveKey="1" >
+            <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Icon
+                        className="trigger"
+                        type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                        onClick={this.toggleCollapsed}
+                        />
+                    </Header>
+
+                    <Content style={{ margin: '24px 16px', padding: 24, background: '#fff', minHeight: 280 }}>
+                        <Tabs defaultActiveKey="1" >
                             <TabPane tab="Pending" key="1">
-                                    <List
-                                    bordered
-                                    dataSource={taskList}
-                                    renderItem={item => (<List.Item onClick={() => this.onTaskClick(item.empId)}>{item.firstName} {item.lastName}</List.Item>)}
-                                />
+                                <List bordered dataSource={taskList} renderItem={item => (<List.Item onClick={() => this.onTaskClick(item.empId)}>{item.firstName} {item.lastName}</List.Item>)}/>
                             </TabPane>
                             <TabPane tab="Completed" key="2">
-                                    <List
-                                    bordered
-                                    dataSource={this.state.dataEmployeeArray}
-                                    renderItem={item => (<List.Item onClick={() => this.onTaskClick(item.empId)}>{item.firstName} {item.lastName}</List.Item>)}
-                                />
+                                <List bordered dataSource={this.state.dataEmployeeArray} renderItem={item => (<List.Item onClick={() => this.onTaskClick(item.empId)}>{item.firstName} {item.lastName}</List.Item>)}/>
                             </TabPane>
                         </Tabs>
-                        
-                        </Col>
-                </Row>
+                    </Content>
+            </Layout>
 
-            </div>
+            </Layout>
          );
     }
 }
